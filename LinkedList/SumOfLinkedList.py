e80342c82da963a14c4807dc4684df049d3a2d22
Dayer <93074807+dayercher005@users.noreply.github.com>
2024-05-19 11:30:04 +0800

    Update SumOfLinkedList.py

--- LinkedList/SumOfLinkedList.py
+++ LinkedList/SumOfLinkedList.py
@@ -1,18 +1,19 @@
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, data):
+        self.data = data
         self.next = None
 
 #Iterative Version
 class LinkedList:
     def __init__(self):
+        self.head = None
 
-    def SumOfLinkedList(head):
+    def SumOfLinkedList(self, data):
         summation = 0
         current = head
         while current:
             current = current.next
-            summation += current.val
+            summation += current.data
 
         return summation
 
